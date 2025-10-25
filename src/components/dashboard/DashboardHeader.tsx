c4152610436b1db4c404fdb9cd1c50c3601b82dd
Rohith Macharla <macharlarohith45@gmail.com>
2025-10-25 07:12:24 +0000

    Try fixing this error: `Runtime Error: Error: React.Children.only expect

--- src/components/dashboard/DashboardHeader.tsx
+++ src/components/dashboard/DashboardHeader.tsx
@@ -23,11 +23,7 @@ export function DashboardHeader() {
   
   return (
     <header className="sticky top-0 z-30 flex h-14 items-center gap-4 border-b bg-background px-4 sm:static sm:h-auto sm:border-0 sm:bg-transparent sm:px-6">
-        <SidebarTrigger asChild>
-            <Button size="icon" variant="outline" className="sm:hidden">
-              <PanelLeft />
-            </Button>
-        </SidebarTrigger>
+        <SidebarTrigger className="sm:hidden" />
         <div className="ml-auto flex items-center gap-2">
             <ThemeToggle />
              {isLoading ? (
